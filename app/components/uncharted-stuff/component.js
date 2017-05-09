234d206d743790419d66ffa9ea7c10f66d001dda
Benjamin Herson <benjaminherson@gmail.com>
2017-05-09 14:45:16 -0700

    add progress wheel w/ hard-coded data and jquery-set percentage value, change how progressbar library saved in package.json

--- app/components/uncharted-stuff/component.js
+++ app/components/uncharted-stuff/component.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
+import hbs from 'htmlbars-inline-precompile';
+import ProgressBar from "npm:progressbar.js";
 
 export default Ember.Component.extend({
+
+  didInsertElement() {
+    const data = { complete: 7, total: 15 };
+    const percentage =  data.complete / data.total;
+    const progCirc = new ProgressBar.Circle(progBarCont, {
+      strokeWidth: 6,
+      easing: 'easeInOut',
+      duration: 1400,
+      color: '#FFEA82',
+      trailColor: '#eee',
+      trailWidth: 1,
+      svgStyle: null
+    });
+
+    $('#circVal').text(String(Math.round(percentage * 100)) + '% complete');
+    progCirc.animate(percentage);
+  }
 });
