ff50abdc95bb6b88a49a011140fc9c9754fd0422
Benjamin Herson <benjaminherson@gmail.com>
2017-05-09 16:03:30 -0700

    add ember-radical and sass, add an ember-radical button w/ connectivity check

--- app/components/uncharted-stuff/component.js
+++ app/components/uncharted-stuff/component.js
@@ -1,17 +1,17 @@
 import Ember from 'ember';
 import hbs from 'htmlbars-inline-precompile';
-import ProgressBar from "npm:progressbar.js";
+import ProgressBar from 'npm:progressbar.js';
+
 
 export default Ember.Component.extend({
-
   didInsertElement() {
     const data = { complete: 7, total: 15 };
     const percentage =  data.complete / data.total;
-    const progCirc = new ProgressBar.Circle(progBarCont, {
+    const progCirc = new ProgressBar.SemiCircle(progBarCont, {
       strokeWidth: 6,
       easing: 'easeInOut',
       duration: 1400,
-      color: '#FFEA82',
+      color: '#FF8100',
       trailColor: '#eee',
       trailWidth: 1,
       svgStyle: null
@@ -19,5 +19,14 @@ export default Ember.Component.extend({
 
     $('#circVal').text(String(Math.round(percentage * 100)) + '% complete');
     progCirc.animate(percentage);
+    console.log(this);
+  },
+
+  actions: {
+    addOne() {
+      console.log('look at me')
+    }
   }
+
+
 });
